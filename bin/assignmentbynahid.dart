da2b09296967be1dcad508f6119db5b34729c6db
Md Nahid Hasan <mdnahidhasanmunna999@gmail.com>
2024-03-15 22:58:52 +0600

    commit

---
+++ bin/assignmentbynahid.dart
@@ -0,0 +1,5 @@
+import 'package:assignmentbynahid/assignmentbynahid.dart' as assignmentbynahid;
+
+void main(List<String> arguments) {
+  print('Hello world: ${assignmentbynahid.calculate()}!');
+}
---
+++ test/assignmentbynahid_test.dart
@@ -0,0 +1,8 @@
+import 'package:assignmentbynahid/assignmentbynahid.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
